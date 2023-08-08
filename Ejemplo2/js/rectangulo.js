0f3c29b750d022c95e7be6eeac8e4bfd68dfa689
Jennifer Zaldaña <jenpatricia003@gmail.com>
2023-08-08 17:19:08 -0600

    initial commit

---
+++ Ejemplo2/js/rectangulo.js
@@ -0,0 +1,5 @@
+var altura= prompt('Introduzca la altura del rectangulo:','');
+var base = prompt('Introduzca el area del rectangulo:','');
+var area;
+area = (base*altura);
+document.write("<header><h1>El área del rectangulo es: " + area + "</h1><hr /><br /></header>");
